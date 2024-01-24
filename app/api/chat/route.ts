7afc9c7a0983da991d07f73b9c6daf2c7c0a8980
mateusloubach <mcesarloubach@gmail.com>
2024-01-24 14:04:22 -0300

    Update - Fixed deployment issue;

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,6 +1,9 @@
 import OpenAI from 'openai';
 import {OpenAIStream, StreamingTextResponse} from 'ai';
 import {AstraDB} from "@datastax/astra-db-ts";
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
